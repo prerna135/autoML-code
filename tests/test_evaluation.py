bfc4249f2a7e9a4c5a9a4346b9d842d7c82bde6a
janvanrijn <janvanrijn@gmail.com>
2018-12-13 18:57:08 -0500

    style

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,13 +1,14 @@
 import evaluation
+import numpy as np
 import unittest
 
 
 class TestEvaluationMethods(unittest.TestCase):
 
     def test_precision_at_n(self):
-        real = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        yhat = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-        exp  = [0.0, 0.0, 0.0, 0.0, 0.0, 2.0/6.0, 4.0/7.0, 6.0/8.0, 8.0/9.0, 1.0]
+        real = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+        yhat = np.array([1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1])
+        exp = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 2.0/6.0, 4.0/7.0, 6.0/8.0, 8.0/9.0, 1.0])
 
         for i in range(len(real)):
             result = evaluation.precision_at_n(real, yhat, i+1)
