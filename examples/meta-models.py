628b88e954629354be6dcb64a376f929f6d8e142
janvanrijn <janvanrijn@gmail.com>
2018-12-13 18:50:23 -0500

    parameterized all other important stuff

--- examples/meta-models.py
+++ examples/meta-models.py
@@ -22,6 +22,11 @@ def parse_args():
     parser.add_argument('--output_directory', type=str,
                         default=os.path.expanduser('~') + '/experiments/meta-models')
     parser.add_argument('--poly_degree', type=int, default=2)
+    parser.add_argument('--precision_at_n', type=int, default=20)
+    parser.add_argument('--precision_out_of_k', type=int, default=100)
+    parser.add_argument('--cv_iterations', type=int, default=5)
+    parser.add_argument('--n_estimators', type=int, default=16)
+    parser.add_argument('--random_seed', type=int, default=42)
     parser.add_argument('--task_limit', type=int, default=None, help='For fast testing')
     args_ = parser.parse_args()
     return args_
@@ -44,15 +49,12 @@ def run(args):
     performances = performances.join(metafeatures, on='task_id', how='inner')
 
     results = []
-    precision_at_n = 20
-    precision_out_of_k = 100
-    precision_name = 'precision_at_%d_out_%d' % (precision_at_n, precision_out_of_k)
-    spearman_name = 'spearmanr_%d' % precision_out_of_k
-    cv_iterations = 5
+    precision_name = 'precision_at_%d_out_%d' % (args.precision_at_n, args.precision_out_of_k)
+    spearman_name = 'spearmanr_%d' % args.precision_out_of_k
 
     # sklearn objects
     quadratic_model = sklearn.linear_model.LinearRegression()
-    random_forest_model = sklearn.ensemble.RandomForestRegressor(n_estimators=16)
+    random_forest_model = sklearn.ensemble.RandomForestRegressor(n_estimators=args.n_estimators, random_state=args.random_seed)
     poly_transform = sklearn.preprocessing.PolynomialFeatures(args.poly_degree)
 
     all_tasks = performances['task_id'].unique()
@@ -75,18 +77,18 @@ def run(args):
         prec_te, prec_tr, spearm_te, spearm_tr = evaluation.cross_validate_surrogate(quadratic_model,
                                                                                      X_poly_test,
                                                                                      frame_task['predictive_accuracy'].values,
-                                                                                     cv_iterations,
-                                                                                     precision_at_n,
-                                                                                     precision_out_of_k)
+                                                                                     args.cv_iterations,
+                                                                                     args.precision_at_n,
+                                                                                     args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'quadratic_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'quadratic_surrogate', 'set': 'train-obs', precision_name: prec_tr, spearman_name: spearm_tr})
 
         prec_te, prec_tr, spearm_te, spearm_tr = evaluation.cross_validate_surrogate(random_forest_model,
                                                                                      frame_task[param_columns].values,
                                                                                      frame_task['predictive_accuracy'].values,
-                                                                                     cv_iterations,
-                                                                                     precision_at_n,
-                                                                                     precision_out_of_k)
+                                                                                     args.cv_iterations,
+                                                                                     args.precision_at_n,
+                                                                                     args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'RF_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'RF_surrogate', 'set': 'train-obs', precision_name: prec_tr, spearman_name: spearm_tr})
 
@@ -96,8 +98,8 @@ def run(args):
                                                                           frame_others['predictive_accuracy'].values,
                                                                           X_poly_test,
                                                                           frame_task['predictive_accuracy'].values,
-                                                                          precision_at_n,
-                                                                          precision_out_of_k)
+                                                                          args.precision_at_n,
+                                                                          args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'quadratic_aggregate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'quadratic_aggregate', 'set': 'train-tasks', precision_name: prec_tr, spearman_name: spearm_tr})
 
@@ -106,8 +108,8 @@ def run(args):
                                                                           frame_others['predictive_accuracy'].values,
                                                                           frame_task[param_columns].values,
                                                                           frame_task['predictive_accuracy'].values,
-                                                                          precision_at_n,
-                                                                          precision_out_of_k)
+                                                                          args.precision_at_n,
+                                                                          args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'RF_aggregate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'RF_aggregate', 'set': 'train-tasks', precision_name: prec_tr, spearman_name: spearm_tr})
 
@@ -117,8 +119,8 @@ def run(args):
                                                                           frame_others['predictive_accuracy'].values,
                                                                           X_poly_meta_test,
                                                                           frame_task['predictive_accuracy'].values,
-                                                                          precision_at_n,
-                                                                          precision_out_of_k)
+                                                                          args.precision_at_n,
+                                                                          args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'quadratic_meta', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'quadratic_meta', 'set': 'train-tasks', precision_name: prec_tr, spearman_name: spearm_tr})
 
@@ -128,8 +130,8 @@ def run(args):
                                                                           frame_others['predictive_accuracy'].values,
                                                                           frame_task[columns].values,
                                                                           frame_task['predictive_accuracy'].values,
-                                                                          precision_at_n,
-                                                                          precision_out_of_k)
+                                                                          args.precision_at_n,
+                                                                          args.precision_out_of_k)
         results.append({'task_id': task_id, 'strategy': 'RF_meta', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'RF_meta', 'set': 'train-tasks', precision_name: prec_tr, spearman_name: spearm_tr})
 
