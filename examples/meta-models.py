02097e5496e2fd23372bf9e72087d58b7d3062f2
janvanrijn <janvanrijn@gmail.com>
2018-12-14 16:05:14 -0500

    added train set for RF meta coef, improved train perf

--- examples/meta-models.py
+++ examples/meta-models.py
@@ -81,6 +81,7 @@ def run(args):
         logging.info('Processing task %d (%d/%d)' % (task_id, idx+1, len(all_tasks)))
         frame_task = performances.loc[performances['task_id'] == task_id]
         frame_others = performances.loc[performances['task_id'] != task_id]
+        coefficients_train_frame = coefficients_data.drop([task_id])
         assert(frame_task.shape[0] > 100)
 
         # some convenience datasets
@@ -96,8 +97,8 @@ def run(args):
                                                                                      args.cv_iterations,
                                                                                      args.precision_at_n,
                                                                                      args.precision_out_of_k)
-        results.append({'task_id': task_id, 'strategy': 'quadratic_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'quadratic_surrogate', 'set': 'train-obs', precision_name: prec_tr, spearman_name: spearm_tr})
+        results.append({'task_id': task_id, 'strategy': 'quadratic_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
 
         prec_te, prec_tr, spearm_te, spearm_tr = evaluation.cross_validate_surrogate(random_forest_model,
                                                                                      frame_task[param_columns].values,
@@ -105,8 +106,8 @@ def run(args):
                                                                                      args.cv_iterations,
                                                                                      args.precision_at_n,
                                                                                      args.precision_out_of_k)
-        results.append({'task_id': task_id, 'strategy': 'RF_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
         results.append({'task_id': task_id, 'strategy': 'RF_surrogate', 'set': 'train-obs', precision_name: prec_tr, spearman_name: spearm_tr})
+        results.append({'task_id': task_id, 'strategy': 'RF_surrogate', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
 
         # aggregates
         prec_te, prec_tr, spearm_te, spearm_tr = evaluation.evaluate_fold(quadratic_model,
@@ -152,14 +153,20 @@ def run(args):
         results.append({'task_id': task_id, 'strategy': 'RF_meta', 'set': 'train-tasks', precision_name: prec_tr, spearman_name: spearm_tr})
 
         # special case: random forest that predicts coefficients of base task
-        random_forest_coef.fit(coefficients_data[metafeatures.columns.values].values,
-                               coefficients_data[quadratic.get_coefficient_names()].values)
-        # note that this code is an almost duplicate from the precision module.
-        y_hat = random_forest_coef.predict(frame_task)
-        rand_indices = np.random.randint(len(frame_task), size=args.precision_out_of_k)
-        prec_te = evaluation.precision_at_n(frame_task['predictive_accuracy'].values[rand_indices], y_hat[rand_indices], args.precision_at_n)
-        spearm_te = scipy.stats.pearsonr(frame_task['predictive_accuracy'].values[rand_indices], y_hat[rand_indices])[0]
+        random_forest_coef.fit(coefficients_train_frame[metafeatures.columns.values].values,
+                               coefficients_train_frame[quadratic.get_coefficient_names()].values)
+        # note that this code is an almost duplicate from the precision module (TODO: refactor)
+        y_hat_te = random_forest_coef.predict(frame_task)
+        rand_indices_te = np.random.randint(len(frame_task), size=args.precision_out_of_k)
+        prec_te = evaluation.precision_at_n(frame_task['predictive_accuracy'].values[rand_indices_te],y_hat_te[rand_indices_te], args.precision_at_n)
+        spearm_te = scipy.stats.pearsonr(frame_task['predictive_accuracy'].values[rand_indices_te], y_hat_te[rand_indices_te])[0]
         results.append({'task_id': task_id, 'strategy': 'RF_meta_coeff', 'set': 'test', precision_name: prec_te, spearman_name: spearm_te})
+        # again, duplicate (TODO: refactor)
+        y_hat_tr = random_forest_coef.predict(frame_task)
+        rand_indices_tr = np.random.randint(len(frame_task), size=args.precision_out_of_k)
+        prec_tr = evaluation.precision_at_n(frame_task['predictive_accuracy'].values[rand_indices_tr], y_hat_tr[rand_indices_tr], args.precision_at_n)
+        spearm_tr = scipy.stats.pearsonr(frame_task['predictive_accuracy'].values[rand_indices_tr], y_hat_tr[rand_indices_tr])[0]
+        results.append({'task_id': task_id, 'strategy': 'RF_meta_coeff', 'set': 'train-task', precision_name: prec_tr, spearman_name: spearm_tr})
 
     result_frame = pd.DataFrame(results)
 
